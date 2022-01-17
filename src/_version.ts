495e3437994773c3847d9d40c2556ecf9883019b
Ana Riera <AnnRiera@outlook.com>
2022-01-17 10:38:56 -0400

    Few steps for connection's implementation (#4)
    * Few steps for connection's implementation
    
    * Comments on PR
    
    * Changes
    
    * Changes related about last comment on PR

---
+++ src/_version.ts
@@ -0,0 +1,1 @@
+export const version = 'harmony-marketplace-sdk/1.0.0'
--- src/base-hr721.ts
+++ src/base-hr721.ts
@@ -1,15 +1,16 @@
+import { JsonRpcProvider } from '@ethersproject/providers'
 
 /**
  * @see {@link https://docs.openzeppelin.com/contracts/3.x/api/token/erc721#IERC721}
  */
-export abstract class  BaseHR721 {
-    async balanceOf(address: string) {}
-    async ownerOf(tokenId: string) {}
-    async safeTransferFrom(fromAddress: string, toAddress: string, tokenId: string) {}
-    async transferFrom(fromAddress: string, toAddress: string, tokenId: string) {}
-    async approve(toAddress: string, tokenId: string){}
-    async getApproved(tokenId: string){}
-    async setApprovalForAll(addressOperator: string, approved: boolean) {}
-    async isApprovedForAll(addressOwner: string, addressOperator: string) {}
-    async safeTransferFromWithData(fromAddress: string, toAddress: string, tokenId: string, data: any) {}
+export abstract class BaseHR721 {
+  abstract balanceOf(address: string): Promise<string>
+  abstract ownerOf(tokenId: string): Promise<string>
+  abstract safeTransferFrom(fromAddress: string, toAddress: string, tokenId: string): Promise<any>
+  abstract transferFrom(fromAddress: string, toAddress: string, tokenId: string): Promise<any>
+  abstract approve(toAddress: string, tokenId: string): Promise<any>
+  abstract getApproved(tokenId: string): Promise<string>
+  abstract setApprovalForAll(addressOperator: string, approved: boolean): Promise<any>
+  abstract isApprovedForAll(addressOwner: string, addressOperator: string): Promise<boolean>
+  abstract safeTransferFromWithData(fromAddress: string, toAddress: string, tokenId: string, data: any): Promise<any>
 }
