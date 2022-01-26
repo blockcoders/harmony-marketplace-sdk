851d070e3993e9a68e82fb15ada74243fa82ff98
Ana Riera <annriera@outlook.com>
2022-01-26 12:38:42 -0400

    Another changes in PR

--- src/hrc721.spec.ts
+++ src/hrc721.spec.ts
@@ -4,7 +4,13 @@ import { expect, use } from 'chai'
 import chaiAsPromised from 'chai-as-promised'
 import sinon from 'sinon'
 import { HRC721 } from './hrc721'
-import { CONTRACT_ABI, CONTRACT_ADDRESS, TEST_ADDRESS, RESULT_TEST_ADDRESS } from './tests/constants'
+import {
+  CONTRACT_ABI,
+  CONTRACT_ADDRESS,
+  TEST_ADDRESS,
+  RESULT_TEST_ADDRESS,
+  RESULT_ORIGIN_ADDRESS,
+} from './tests/constants'
 
 describe('HarmonyProvider', () => {
   const client = new Harmony('https://api.s0.b.hmny.io/', {
@@ -32,7 +38,7 @@ describe('HarmonyProvider', () => {
       expect(balance).to.exist
       expect(balance).to.not.be.null
       expect(balance).to.not.be.undefined
-      expect(balance).to.be.equals(0)
+      expect(balance).to.be.equals(3)
     }).timeout(5000)
 
     it('should throw an error if address is not provided', async () => {
@@ -48,6 +54,18 @@ describe('HarmonyProvider', () => {
       expect(owner).to.not.be.undefined
       expect(owner).to.be.equals(RESULT_TEST_ADDRESS)
     }).timeout(5000)
+
+    it('should returns the owner of the tokenId token in an account without NFT.', async () => {
+      const owner = await provider.ownerOf('0')
+      expect(owner).to.exist
+      expect(owner).to.not.be.null
+      expect(owner).to.not.be.undefined
+      expect(owner).to.be.equals(RESULT_ORIGIN_ADDRESS)
+    }).timeout(5000)
+
+    it('should throw an error if tokenId is not valid', async () => {
+      expect(provider.ownerOf('6')).to.be.rejectedWith(Error)
+    })
 
     it('should throw an error if tokenId is not provided', async () => {
       expect(provider.ownerOf('')).to.be.rejectedWith(Error)
