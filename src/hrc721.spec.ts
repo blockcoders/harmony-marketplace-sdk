04cebe69bbd2aac882aafa55b5f75990a50a2943
Brian Zuker <brianzuker@gmail.com>
2022-01-31 17:11:07 -0300

    Feat/safe transfer from (#16)
    * add harmony account package
    
    * add transferFrom
    
    * fixes

--- src/hrc721.spec.ts
+++ src/hrc721.spec.ts
@@ -1,4 +1,5 @@
 import { Harmony } from '@harmony-js/core'
+import { TxStatus } from '@harmony-js/transaction'
 import { ChainID, ChainType } from '@harmony-js/utils'
 import { expect, use } from 'chai'
 import chaiAsPromised from 'chai-as-promised'
@@ -7,9 +8,11 @@ import { HRC721 } from './hrc721'
 import {
   CONTRACT_ABI,
   CONTRACT_ADDRESS,
-  TEST_ADDRESS,
+  TEST_ADDRESS_1,
   RESULT_TEST_ADDRESS,
   RESULT_ORIGIN_ADDRESS,
+  TEST_ACCOUNT_2,
+  TEST_ACCOUNT_3,
   EMPTY_TEST_ADDRESS,
 } from './tests/constants'
 
@@ -35,7 +38,7 @@ describe('HarmonyProvider', () => {
 
   describe('balanceOf', () => {
     it('should get the number of tokens in the specified account', async () => {
-      const balance = await provider.balanceOf(TEST_ADDRESS)
+      const balance = await provider.balanceOf(TEST_ADDRESS_1)
       expect(balance).to.exist
       expect(balance).to.not.be.null
       expect(balance).to.not.be.undefined
@@ -73,6 +76,49 @@ describe('HarmonyProvider', () => {
     })
   })
 
+  describe('transferFrom', () => {
+    it('should throw if there is no signer', () => {
+      expect(provider.transferFrom(TEST_ADDRESS_1, RESULT_TEST_ADDRESS, '1')).to.be.rejectedWith(Error)
+    })
+
+    it('should transfer the ownership of a token from one address to another', async () => {
+      const owner = await provider.ownerOf('5')
+
+      const ownerAccount = [TEST_ACCOUNT_2, TEST_ACCOUNT_3].find((account) => account.address === owner)
+      const receiverAccount = [TEST_ACCOUNT_2, TEST_ACCOUNT_3].find((account) => account.address !== owner)
+      if (!ownerAccount || !receiverAccount) throw new Error('Owner or receiver not found')
+
+      provider.setSignerByPrivateKey(ownerAccount.privateKey)
+      const result = await provider.transferFrom(ownerAccount.address, receiverAccount.address, '5')
+
+      expect(result.txStatus).to.eq(TxStatus.CONFIRMED)
+      expect(result.receipt).to.exist
+      expect(result.receipt?.blockHash).to.be.string
+    })
+  })
+
+  describe.skip('safeTransferFrom', () => {
+    it('should throw if there is no signer', () => {
+      expect(provider.safeTransferFrom(TEST_ADDRESS_1, RESULT_TEST_ADDRESS, '1')).to.be.rejectedWith(Error)
+    })
+
+    it('should transfer the ownership of a token from one address to another', async () => {
+      const owner = await provider.ownerOf('5')
+      expect(owner).to.be.oneOf([TEST_ACCOUNT_2.address, TEST_ACCOUNT_3.address])
+
+      const ownerAccount = [TEST_ACCOUNT_2, TEST_ACCOUNT_3].find((account) => account.address === owner)
+      const receiverAccount = [TEST_ACCOUNT_2, TEST_ACCOUNT_3].find((account) => account.address !== owner)
+      if (!ownerAccount || !receiverAccount) throw new Error('Account not found')
+
+      provider.setSignerByPrivateKey(ownerAccount.privateKey)
+      const result = await provider.safeTransferFrom(ownerAccount.address, receiverAccount.address, '5')
+
+      expect(result.txStatus).to.eq(TxStatus.CONFIRMED)
+      expect(result.receipt).to.exist
+      expect(result.receipt?.blockHash).to.be.string
+    })
+  })
+
   // TODO: add more tests when the approve function works
   describe('getApproved', () => {
     it('should return the account approved for tokenId token', async () => {
@@ -90,7 +136,7 @@ describe('HarmonyProvider', () => {
 
   describe('isApprovedForAll', () => {
     it('should return a boolean value if the operator is allowed to manage all of the assets of owner', async () => {
-      const approved = await provider.isApprovedForAll(TEST_ADDRESS, EMPTY_TEST_ADDRESS)
+      const approved = await provider.isApprovedForAll(TEST_ADDRESS_1, EMPTY_TEST_ADDRESS)
       expect(approved).to.exist
       expect(approved).to.not.be.null
       expect(approved).to.not.be.undefined
@@ -102,7 +148,7 @@ describe('HarmonyProvider', () => {
     })
 
     it('should throw an error if addressOperator is not provided', async () => {
-      expect(provider.isApprovedForAll(TEST_ADDRESS, '')).to.be.rejectedWith(Error)
+      expect(provider.isApprovedForAll(TEST_ADDRESS_1, '')).to.be.rejectedWith(Error)
     })
 
     it('should throw an error if params are not provided', async () => {
