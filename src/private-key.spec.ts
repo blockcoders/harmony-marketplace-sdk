32c61d7ebdb9f190eec22b3c812cebaee9447106
Ana Riera <annriera@outlook.com>
2022-02-28 12:52:48 -0400

    Changes of test structure

--- src/private-key.spec.ts
+++ src/private-key.spec.ts
@@ -1,81 +1,38 @@
-import { Wallet } from '@harmony-js/account'
-import { HttpProvider, WSProvider } from '@harmony-js/network'
 import { ChainID } from '@harmony-js/utils'
 import { expect } from 'chai'
-import { HARMONY_RPC_SHARD_0_TESTNET, HARMONY_RPC_SHARD_1_TESTNET, HARMONY_RPC_TESTNET_WS } from './constants'
-import { HarmonyShards } from './interfaces'
-// import { Key } from './key'
+import { HARMONY_RPC_SHARD_0_TESTNET } from './constants'
+import { Key } from './key'
 import { PrivateKey } from './private-key'
 import { TEST_ACCOUNT_1 } from './tests/constants'
 
 describe('Private Key Class', () => {
   let instance: PrivateKey
 
-  it('should be an instance of Wallet', async () => {
+  it('should be an instance of Key', async () => {
     instance = new PrivateKey(HARMONY_RPC_SHARD_0_TESTNET.url, TEST_ACCOUNT_1.privateKey)
 
     expect(instance).to.not.be.null
     expect(instance).to.not.be.undefined
-    expect(instance).to.be.instanceOf(Wallet)
+    expect(instance).to.be.instanceOf(Key)
   })
 
-  it('should instance correctly the private key class using url as a string with chain id', async () => {
+  it('should get the signer associated', async () => {
     instance = new PrivateKey(HARMONY_RPC_SHARD_0_TESTNET.url, TEST_ACCOUNT_1.privateKey, ChainID.HmyTestnet)
 
     expect(instance).to.not.be.null
     expect(instance).to.not.be.undefined
-    expect(instance).to.be.instanceOf(PrivateKey)
+    expect(instance.signer).to.exist
+    expect(instance.signer?.address).to.exist
+    expect(instance.signer?.address).to.be.equal(TEST_ACCOUNT_1.address)
   })
 
-  it('should instance correctly the private key class using url as a string', async () => {
-    instance = new PrivateKey(HARMONY_RPC_SHARD_1_TESTNET.url, TEST_ACCOUNT_1.privateKey)
+  it('should get the private key associated', async () => {
+    instance = new PrivateKey(HARMONY_RPC_SHARD_0_TESTNET.url, TEST_ACCOUNT_1.privateKey, ChainID.HmyTestnet)
 
     expect(instance).to.not.be.null
     expect(instance).to.not.be.undefined
-    expect(instance).to.be.instanceOf(PrivateKey)
-  })
-
-  it('should instance correctly the private key class using url as a HttpProvider', async () => {
-    const http = new HttpProvider(HARMONY_RPC_SHARD_0_TESTNET.url)
-    instance = new PrivateKey(http, TEST_ACCOUNT_1.privateKey, ChainID.HmyTestnet)
-
-    expect(instance).to.not.be.null
-    expect(instance).to.not.be.undefined
-    expect(instance).to.be.instanceOf(PrivateKey)
-  })
-
-  it('should instance correctly the private key class using url as a WSProvider', async () => {
-    const http = new WSProvider(HARMONY_RPC_TESTNET_WS)
-    instance = new PrivateKey(http, TEST_ACCOUNT_1.privateKey, ChainID.HmyTestnet)
-
-    expect(instance).to.not.be.null
-    expect(instance).to.not.be.undefined
-    expect(instance).to.be.instanceOf(PrivateKey)
-  })
-
-  it('should instance correctly the private key class using url as a HarmonyShards', async () => {
-    instance = new PrivateKey(HarmonyShards.SHARD_0_TESTNET, TEST_ACCOUNT_1.privateKey, ChainID.HmyTestnet)
-
-    expect(instance).to.not.be.null
-    expect(instance).to.not.be.undefined
-    expect(instance).to.be.instanceOf(PrivateKey)
-  })
-
-  it('should throw an error if url is not provided', async () => {
-    try {
-      new PrivateKey('', TEST_ACCOUNT_1.privateKey)
-    } catch (error) {
-      expect(error).to.be.exist
-      expect(error).to.be.instanceOf(Error)
-    }
-  })
-
-  it('should throw an error if privateKey is not provided', async () => {
-    try {
-      new PrivateKey(HarmonyShards.SHARD_0_TESTNET, '')
-    } catch (error) {
-      expect(error).to.be.exist
-      expect(error).to.be.instanceOf(Error)
-    }
+    expect(instance.signer).to.exist
+    expect(instance.signer?.privateKey).to.exist
+    expect(instance.signer?.privateKey).to.be.equal(`0x${TEST_ACCOUNT_1.privateKey}`)
   })
 })
