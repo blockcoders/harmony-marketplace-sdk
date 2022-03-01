80f3823110c17d1a933869444ee104e7ea860ec0
Ana Riera <AnnRiera@outlook.com>
2022-03-01 14:15:17 -0400

    Feature/mnemonicKey (#39)
    * Mnemonic key class tests
    
    * Covering another line in mnemonic key file

--- src/tests/constants.ts
+++ src/tests/constants.ts
@@ -4,6 +4,7 @@ export const HARMONY_TESTNET = 'https://api.s0.b.hmny.io/'
 export const TEST_ADDRESS_1 = '0x7c61c8f282aeaae3e0476d0533d21dc42c5aa146'.toLowerCase()
 export const TEST_ADDRESS_2 = '0xc57DE96E7ADd6D0fc6BdF51758Fd90b111f89b79'.toLowerCase()
 export const TEST_ADDRESS_3 = '0x264F47C0E2fb1B587F6fa270862F3CBc68460B75'.toLowerCase()
+export const TEST_ADDRESS_4 = '0xe8f887475466f6a232f428f038b9ada36dc3582d'.toLowerCase()
 
 export const TEST_ACCOUNT_1 = {
   address: TEST_ADDRESS_1,
@@ -16,6 +17,11 @@ export const TEST_ACCOUNT_2 = {
 export const TEST_ACCOUNT_3 = {
   address: TEST_ADDRESS_3,
   privateKey: '621b40f892817b902ed5c9bef2e0bae9ba9dbc3868a8bb361493be08f1eca70f'.toLowerCase(),
+}
+export const TEST_ACCOUNT_4 = {
+  address: TEST_ADDRESS_4,
+  privateKey: '0x8a91aa24d25d221a9c213a0ac40fc88c0771fac5f6faa117688e6230a2e7d1d8'.toLowerCase(),
+  mnemonic: 'build surprise shadow win over science walk income mosquito armed key own',
 }
 export const EMPTY_TEST_ADDRESS = '0x36f41b8a79eca329610d6158f3ea9676bec281b9'.toLowerCase()
 export const HRC721_CONTRACT_ADDRESS = '0xF00373c538cca8ac7f2290ffFA425c32459ef10b'.toLowerCase()
