cd3970ec798c1370eef5636c4c7788dadf9059e8
Ana Riera <annriera@outlook.com>
2022-03-08 15:37:18 -0400

    Tests for HRC1155, HRC721 and BaseToken

--- src/base-token.spec.ts
+++ src/base-token.spec.ts
@@ -1,13 +1,23 @@
+import { Account } from '@harmony-js/account'
 import { ChainID } from '@harmony-js/utils'
 import BN from 'bn.js'
 import { expect, use } from 'chai'
 import chaiAsPromised from 'chai-as-promised'
 import sinon from 'sinon'
 import { BaseToken } from './base-token'
-import { AddressZero } from './constants'
+import { AddressZero, HARMONY_RPC_SHARD_0_TESTNET } from './constants'
 import { BNish, HarmonyShards, ITransactionOptions } from './interfaces'
+import { MnemonicKey } from './mnemonic-key'
 import { PrivateKey } from './private-key'
-import { TEST_ADDRESS_1, EMPTY_TEST_ADDRESS, TEST_ADDRESS_2, TEST_PK_1 } from './tests/constants'
+import {
+  TEST_ADDRESS_1,
+  EMPTY_TEST_ADDRESS,
+  TEST_ADDRESS_2,
+  TEST_PK_1,
+  TX_OPTIONS,
+  TOKEN_GOLD,
+  TEST_SEED,
+} from './tests/constants'
 import { ABI } from './tests/contracts/HRC721/abi'
 
 class TestToken extends BaseToken {
@@ -93,25 +103,96 @@ describe('Base Token Provider', () => {
   })
 
   describe('setSignerByPrivateKey', () => {
-    it('should throw an error if privateKey is not provided', async () => {
-      const stub = sinon.stub(BaseToken.prototype, 'setSignerByPrivateKey')
-      stub.withArgs('').onCall(0).rejects()
+    it('should return an instance of an Account', async () => {
+      const account = contract.setSignerByPrivateKey(TEST_PK_1)
 
-      expect(contract.setSignerByPrivateKey('')).to.be.rejectedWith(Error)
+      expect(account).to.not.be.null
+      expect(account).to.not.be.undefined
+      expect(account).to.be.instanceOf(Account)
+    })
+
+    it('should throw an error if privateKey is not provided', async () => {
+      try {
+        contract.setSignerByPrivateKey('')
+      } catch (error) {
+        expect(error).to.not.null
+        expect(error).to.not.undefined
+        expect(error).to.be.instanceOf(Error)
+      }
     })
 
     it('should throw an error if privateKey is not valid', async () => {
-      const stub = sinon.stub(BaseToken.prototype, 'setSignerByPrivateKey')
-      stub.withArgs('This is a test').onCall(0).rejects()
-
-      expect(contract.setSignerByPrivateKey('This is a test')).to.be.rejectedWith(Error)
+      try {
+        contract.setSignerByPrivateKey('This is a test')
+      } catch (error) {
+        expect(error).to.not.null
+        expect(error).to.not.undefined
+        expect(error).to.be.instanceOf(Error)
+      }
     })
 
-    it('should throw an error if type is not provided', async () => {
-      const stub = sinon.stub(BaseToken.prototype, 'setSignerByPrivateKey')
-      stub.withArgs(TEST_ADDRESS_2).onCall(0).rejects()
+    it('should throw an error if type is not valid', async () => {
+      try {
+        contract.setSignerByPrivateKey(TEST_ADDRESS_2)
+      } catch (error) {
+        expect(error).to.not.null
+        expect(error).to.not.undefined
+        expect(error).to.be.instanceOf(Error)
+      }
+    })
+  })
 
-      expect(contract.setSignerByPrivateKey(TEST_ADDRESS_2)).to.be.rejectedWith(Error)
+  describe('send', () => {
+    it('should throw an error if method is invalid', async () => {
+      expect(contract.send('test', [TOKEN_GOLD], TX_OPTIONS)).to.be.rejectedWith(Error)
+    })
+
+    it('should throw an error if there is no transaction response', async () => {
+      const stub = sinon.stub(BaseToken.prototype, 'send')
+      stub.withArgs('safeTransferFrom', [TEST_ADDRESS_2, '', TOKEN_GOLD, 10, '0x'], TX_OPTIONS).onFirstCall().rejects()
+
+      expect(
+        contract.send('safeTransferFrom', [TEST_ADDRESS_2, '', TOKEN_GOLD, 10, '0x'], TX_OPTIONS),
+      ).to.be.rejectedWith(Error)
+    })
+
+    it('should throw an error if there is no params', async () => {
+      const stub = sinon.stub(BaseToken.prototype, 'send')
+      stub.withArgs('', []).onFirstCall().rejects()
+
+      expect(contract.send('', [])).to.be.rejectedWith(Error)
+    })
+  })
+
+  describe('setSignerByMnemonic', () => {
+    it('should return an Account instance', async () => {
+      const account = contract.setSignerByMnemonic(TEST_SEED)
+
+      expect(account).to.not.be.null
+      expect(account).to.not.be.undefined
+      expect(account).to.be.instanceOf(Account)
+    })
+
+    it('should throw an error if mnemonic is not valid', async () => {
+      try {
+        contract.setSignerByMnemonic('')
+      } catch (error) {
+        expect(error).to.not.null
+        expect(error).to.not.undefined
+        expect(error).to.be.instanceOf(Error)
+      }
+    })
+  })
+
+  describe('setSignerByKey', () => {
+    it('should throw an error if key is not valid', async () => {
+      try {
+        new MnemonicKey(HARMONY_RPC_SHARD_0_TESTNET.url, {}, ChainID.HmyTestnet)
+      } catch (error) {
+        expect(error).to.not.null
+        expect(error).to.not.undefined
+        expect(error).to.be.instanceOf(Error)
+      }
     })
   })
 })
--- src/hrc721.spec.ts
+++ src/hrc721.spec.ts
@@ -1,4 +1,3 @@
-import BN from 'bn.js'
 import { expect, use } from 'chai'
 import chaiAsPromised from 'chai-as-promised'
 import sinon from 'sinon'
@@ -44,7 +43,7 @@ describe('HRC721 Contract Interface', () => {
       const stub = sinon.stub(contract, 'call').withArgs('balanceOf', [TEST_ADDRESS_1], TX_OPTIONS)
       stub.resolves().returns(FAKE_BALANCE_HRC721)
 
-      const call = await contract.call<BN>('balanceOf', [TEST_ADDRESS_1], TX_OPTIONS)
+      const call = await contract.balanceOf(TEST_ADDRESS_1, TX_OPTIONS)
 
       expect(stub.calledOnce).to.be.true
       expect(stub.callCount).to.be.equals(1)
@@ -55,14 +54,7 @@ describe('HRC721 Contract Interface', () => {
       const stub = sinon.stub(contract, 'call')
       stub.withArgs('balanceOf', [], TX_OPTIONS).onFirstCall().rejects()
 
-      expect(contract.call<BN>('balanceOf', [], TX_OPTIONS)).to.be.rejectedWith(Error)
-    })
-
-    it('should throw an error if txOptions is not provided', async () => {
-      const stub = sinon.stub(contract, 'call')
-      stub.withArgs('balanceOf', [TEST_ADDRESS_1]).onFirstCall().rejects()
-
-      expect(contract.call<BN>('balanceOf', [TEST_ADDRESS_1])).to.be.rejectedWith(Error)
+      expect(contract.balanceOf('', TX_OPTIONS)).to.be.rejectedWith(Error)
     })
   })
 
@@ -70,7 +62,7 @@ describe('HRC721 Contract Interface', () => {
     it('should return the owner of the tokenId token', async () => {
       const stub = sinon.stub(contract, 'call').withArgs('ownerOf', [TOKEN_GOLD], TX_OPTIONS)
       stub.resolves().returns(FAKE_OWNER_HRC721)
-      const owner = await contract.call<string>('ownerOf', [TOKEN_GOLD], TX_OPTIONS)
+      const owner = await contract.ownerOf(TOKEN_GOLD, TX_OPTIONS)
 
       expect(stub.calledOnce).to.be.true
       expect(stub.callCount).to.be.equals(1)
@@ -80,7 +72,7 @@ describe('HRC721 Contract Interface', () => {
     it('should return the owner of the tokenId token with tokenId as a string', async () => {
       const stub = sinon.stub(contract, 'call').withArgs('ownerOf', [TOKEN_GOLD.toString()], TX_OPTIONS)
       stub.resolves().returns(FAKE_OWNER_HRC721)
-      const owner = await contract.call<string>('ownerOf', [TOKEN_GOLD.toString()], TX_OPTIONS)
+      const owner = await contract.ownerOf(TOKEN_GOLD.toString(), TX_OPTIONS)
 
       expect(stub.calledOnce).to.be.true
       expect(stub.callCount).to.be.equals(1)
@@ -90,7 +82,7 @@ describe('HRC721 Contract Interface', () => {
     it('should return the origin address of the tokenId token if the token has no owner', async () => {
       const stub = sinon.stub(contract, 'call').withArgs('ownerOf', [TOKEN_SWORD], TX_OPTIONS)
       stub.resolves().returns(FAKE_OWNER_HRC721)
-      const owner = await contract.call<string>('ownerOf', [TOKEN_SWORD], TX_OPTIONS)
+      const owner = await contract.ownerOf(TOKEN_SWORD, TX_OPTIONS)
 
       expect(stub.calledOnce).to.be.true
       expect(stub.callCount).to.be.equals(1)
@@ -101,21 +93,21 @@ describe('HRC721 Contract Interface', () => {
       const stub = sinon.stub(contract, 'call').withArgs('ownerOf', [6], TX_OPTIONS)
       stub.rejects()
 
-      expect(contract.call<string>('ownerOf', [6], TX_OPTIONS)).to.be.rejectedWith(Error)
+      expect(contract.ownerOf(6, TX_OPTIONS)).to.be.rejectedWith(Error)
     })
 
     it('should throw an error if tokenId is not provided', async () => {
       const stub = sinon.stub(contract, 'call').withArgs('ownerOf')
       stub.rejects()
 
-      expect(contract.call<string>('ownerOf')).to.be.rejectedWith(Error)
+      expect(contract.ownerOf('')).to.be.rejectedWith(Error)
     })
 
     it('should throw an error if txOptions is not provided', async () => {
       const stub = sinon.stub(contract, 'call').withArgs('ownerOf', [TOKEN_GOLD])
       stub.rejects()
 
-      expect(contract.call<string>('ownerOf', [TOKEN_GOLD])).to.be.rejectedWith(Error)
+      expect(contract.ownerOf(TOKEN_GOLD)).to.be.rejectedWith(Error)
     })
   })
 
@@ -128,7 +120,7 @@ describe('HRC721 Contract Interface', () => {
         .resolves()
         .returns(FAKE_TX_HRC721)
 
-      await contract.send('transferFrom', [TEST_ADDRESS_2, TEST_ADDRESS_1, TOKEN_GOLD], TX_OPTIONS)
+      await contract.transferFrom(TEST_ADDRESS_2, TEST_ADDRESS_1, TOKEN_GOLD, TX_OPTIONS)
 
       expect(stub.calledOnce).to.be.true
       expect(stub.callCount).to.be.equals(1)
@@ -138,21 +130,21 @@ describe('HRC721 Contract Interface', () => {
       const stub = sinon.stub(contract, 'send').withArgs('transferFrom', ['', TEST_ADDRESS_1, TOKEN_GOLD], TX_OPTIONS)
       stub.rejects()
 
-      expect(contract.send('transferFrom', ['', TEST_ADDRESS_1, TOKEN_GOLD], TX_OPTIONS)).to.be.rejectedWith(Error)
+      expect(contract.transferFrom('', TEST_ADDRESS_1, TOKEN_GOLD, TX_OPTIONS)).to.be.rejectedWith(Error)
     })
 
     it('should throw an error if there is no receiver', () => {
       const stub = sinon.stub(contract, 'send').withArgs('transferFrom', [TEST_ADDRESS_2, '', TOKEN_GOLD], TX_OPTIONS)
       stub.rejects()
 
-      expect(contract.send('transferFrom', [TEST_ADDRESS_2, '', TOKEN_GOLD], TX_OPTIONS)).to.be.rejectedWith(Error)
+      expect(contract.transferFrom(TEST_ADDRESS_2, '', TOKEN_GOLD, TX_OPTIONS)).to.be.rejectedWith(Error)
     })
 
     it('should throw an error if there is no tokenId', () => {
       const stub = sinon.stub(contract, 'send').withArgs('transferFrom', [TEST_ADDRESS_2, TEST_ADDRESS_1], TX_OPTIONS)
       stub.rejects()
 
-      expect(contract.send('transferFrom', [TEST_ADDRESS_2, TEST_ADDRESS_1], TX_OPTIONS)).to.be.rejectedWith(Error)
+      expect(contract.transferFrom(TEST_ADDRESS_2, TEST_ADDRESS_1, '', TX_OPTIONS)).to.be.rejectedWith(Error)
     })
   })
 
@@ -163,7 +155,7 @@ describe('HRC721 Contract Interface', () => {
         .withArgs('safeTransferFrom', [TEST_ADDRESS_1, TEST_ADDRESS_2, TOKEN_GOLD, '0x'], TX_OPTIONS)
       stub.resolves()
 
-      await contract.send('safeTransferFrom', [TEST_ADDRESS_1, TEST_ADDRESS_2, TOKEN_GOLD, '0x'], TX_OPTIONS)
+      await contract.safeTransferFrom(TEST_ADDRESS_1, TEST_ADDRESS_2, TOKEN_GOLD, '0x', TX_OPTIONS)
 
       expect(stub.calledOnce).to.be.true
       expect(stub.callCount).to.be.equals(1)
@@ -175,7 +167,7 @@ describe('HRC721 Contract Interface', () => {
         .withArgs('safeTransferFrom', [TEST_ADDRESS_1, TEST_ADDRESS_2, TOKEN_GOLD], TX_OPTIONS)
       stub.resolves()
 
-      await contract.send('safeTransferFrom', [TEST_ADDRESS_1, TEST_ADDRESS_2, TOKEN_GOLD], TX_OPTIONS)
+      await contract.safeTransferFrom(TEST_ADDRESS_1, TEST_ADDRESS_2, TOKEN_GOLD, '', TX_OPTIONS)
 
       expect(stub.calledOnce).to.be.true
       expect(stub.callCount).to.be.equals(1)
@@ -185,25 +177,21 @@ describe('HRC721 Contract Interface', () => {
       const stub = sinon.stub(contract, 'send')
       stub.withArgs('safeTransferFrom', ['', TEST_ADDRESS_2, TOKEN_GOLD, '0x'], TX_OPTIONS).onFirstCall().rejects()
 
-      expect(contract.send('safeTransferFrom', ['', TEST_ADDRESS_2, TOKEN_GOLD, '0x'], TX_OPTIONS)).to.be.rejectedWith(
-        Error,
-      )
+      expect(contract.safeTransferFrom('', TEST_ADDRESS_2, TOKEN_GOLD, '0x', TX_OPTIONS)).to.be.rejectedWith(Error)
     })
 
     it('should throw if there is no receiver', () => {
       const stub = sinon.stub(contract, 'send')
       stub.withArgs('safeTransferFrom', [TEST_ADDRESS_1, '', TOKEN_GOLD, '0x'], TX_OPTIONS).onFirstCall().rejects()
 
-      expect(contract.send('safeTransferFrom', [TEST_ADDRESS_1, '', TOKEN_GOLD, '0x'], TX_OPTIONS)).to.be.rejectedWith(
-        Error,
-      )
+      expect(contract.safeTransferFrom(TEST_ADDRESS_1, '', TOKEN_GOLD, '0x', TX_OPTIONS)).to.be.rejectedWith(Error)
     })
 
     it('should throw if there is no tokenId', () => {
       const stub = sinon.stub(contract, 'send')
       stub.withArgs('safeTransferFrom', [TEST_ADDRESS_1, TEST_ADDRESS_2], TX_OPTIONS).onFirstCall().rejects()
 
-      expect(contract.send('safeTransferFrom', [TEST_ADDRESS_1, TEST_ADDRESS_2], TX_OPTIONS)).to.be.rejectedWith(Error)
+      expect(contract.safeTransferFrom(TEST_ADDRESS_1, TEST_ADDRESS_2, '', TX_OPTIONS)).to.be.rejectedWith(Error)
     })
   })
 
@@ -213,7 +201,7 @@ describe('HRC721 Contract Interface', () => {
       stub.resolves().returns(FAKE_APPROVED_HRC721)
       const mockedResponse = await FAKE_APPROVED_HRC721
 
-      const approved = await contract.call<string>('getApproved', [TOKEN_GOLD], TX_OPTIONS)
+      const approved = await contract.getApproved(TOKEN_GOLD, TX_OPTIONS)
 
       expect(stub.calledOnce).to.be.true
       expect(stub.callCount).to.be.equals(1)
@@ -225,7 +213,7 @@ describe('HRC721 Contract Interface', () => {
       stub.resolves().returns(FAKE_APPROVED_HRC721)
       const mockedResponse = await FAKE_APPROVED_HRC721
 
-      const approved = await contract.call<string>('getApproved', [TOKEN_GOLD])
+      const approved = await contract.getApproved(TOKEN_GOLD)
 
       expect(stub.calledOnce).to.be.true
       expect(stub.callCount).to.be.equals(1)
@@ -236,14 +224,14 @@ describe('HRC721 Contract Interface', () => {
       const stub = sinon.stub(contract, 'call')
       stub.withArgs('getApproved', [], TX_OPTIONS).onFirstCall().rejects()
 
-      expect(contract.call<string>('getApproved', [], TX_OPTIONS)).to.be.rejectedWith(Error)
+      expect(contract.getApproved('', TX_OPTIONS)).to.be.rejectedWith(Error)
     })
 
     it('should throw an error if tokenId is invalid', async () => {
       const stub = sinon.stub(contract, 'call')
       stub.withArgs('getApproved', [6], TX_OPTIONS).onFirstCall().rejects()
 
-      expect(contract.call<string>('getApproved', [6], TX_OPTIONS)).to.be.rejectedWith(Error)
+      expect(contract.getApproved(6, TX_OPTIONS)).to.be.rejectedWith(Error)
     })
   })
 
@@ -255,7 +243,7 @@ describe('HRC721 Contract Interface', () => {
       stub.resolves().returns(FAKE_IS_APPROVED_HRC721)
       const mockedResponse = await FAKE_IS_APPROVED_HRC721
 
-      const approved = await contract.call('isApprovedForAll', [TEST_ADDRESS_1, EMPTY_TEST_ADDRESS], TX_OPTIONS)
+      const approved = await contract.isApprovedForAll(TEST_ADDRESS_1, EMPTY_TEST_ADDRESS, TX_OPTIONS)
 
       expect(stub.calledOnce).to.be.true
       expect(stub.callCount).to.be.equals(1)
@@ -267,7 +255,7 @@ describe('HRC721 Contract Interface', () => {
       stub.resolves().returns(FAKE_IS_APPROVED_HRC721)
       const mockedResponse = await FAKE_IS_APPROVED_HRC721
 
-      const approved = await contract.call('isApprovedForAll', [TEST_ADDRESS_1, EMPTY_TEST_ADDRESS])
+      const approved = await contract.isApprovedForAll(TEST_ADDRESS_1, EMPTY_TEST_ADDRESS)
 
       expect(stub.calledOnce).to.be.true
       expect(stub.callCount).to.be.equals(1)
@@ -278,35 +266,35 @@ describe('HRC721 Contract Interface', () => {
       const stub = sinon.stub(contract, 'call')
       stub.withArgs('isApprovedForAll', ['', EMPTY_TEST_ADDRESS], TX_OPTIONS).onFirstCall().rejects()
 
-      expect(contract.call('isApprovedForAll', ['', EMPTY_TEST_ADDRESS], TX_OPTIONS)).to.be.rejectedWith(Error)
+      expect(contract.isApprovedForAll('', EMPTY_TEST_ADDRESS, TX_OPTIONS)).to.be.rejectedWith(Error)
     })
 
     it('should throw an error if addressOperator is not provided', async () => {
       const stub = sinon.stub(contract, 'call')
       stub.withArgs('isApprovedForAll', [TEST_ADDRESS_1, ''], TX_OPTIONS).onFirstCall().rejects()
 
-      expect(contract.call('isApprovedForAll', [TEST_ADDRESS_1, ''], TX_OPTIONS)).to.be.rejectedWith(Error)
+      expect(contract.isApprovedForAll(TEST_ADDRESS_1, '', TX_OPTIONS)).to.be.rejectedWith(Error)
     })
 
     it('should throw an error if addressOwner is zero-address', async () => {
       const stub = sinon.stub(contract, 'call')
       stub.withArgs('isApprovedForAll', [AddressZero, EMPTY_TEST_ADDRESS]).onFirstCall().rejects()
 
-      expect(contract.call('isApprovedForAll', [AddressZero, EMPTY_TEST_ADDRESS])).to.be.rejectedWith(Error)
+      expect(contract.isApprovedForAll(AddressZero, EMPTY_TEST_ADDRESS)).to.be.rejectedWith(Error)
     })
 
     it('should throw an error if addressOperator is zero-address', async () => {
       const stub = sinon.stub(contract, 'call')
       stub.withArgs('isApprovedForAll', [TEST_ADDRESS_1, AddressZero]).onFirstCall().rejects()
 
-      expect(contract.call('isApprovedForAll', [TEST_ADDRESS_1, AddressZero])).to.be.rejectedWith(Error)
+      expect(contract.isApprovedForAll(TEST_ADDRESS_1, AddressZero)).to.be.rejectedWith(Error)
     })
 
     it('should throw an error if params are not provided', async () => {
       const stub = sinon.stub(contract, 'call')
       stub.withArgs('isApprovedForAll', ['', '']).onFirstCall().rejects()
 
-      expect(contract.call('isApprovedForAll', ['', ''])).to.be.rejectedWith(Error)
+      expect(contract.isApprovedForAll('', '')).to.be.rejectedWith(Error)
     })
   })
 
@@ -315,7 +303,7 @@ describe('HRC721 Contract Interface', () => {
       const stub = sinon.stub(contract, 'send').withArgs('setApprovalForAll', [TEST_ADDRESS_1, true], TX_OPTIONS)
       stub.resolves()
 
-      await contract.send('setApprovalForAll', [TEST_ADDRESS_1, true], TX_OPTIONS)
+      await contract.setApprovalForAll(TEST_ADDRESS_1, true, TX_OPTIONS)
       expect(stub.calledOnce).to.be.true
       expect(stub.callCount).to.be.equals(1)
     })
@@ -324,28 +312,67 @@ describe('HRC721 Contract Interface', () => {
       const stub = sinon.stub(contract, 'send').withArgs('setApprovalForAll', ['', true], TX_OPTIONS)
       stub.rejects()
 
-      expect(contract.send('setApprovalForAll', ['', true], TX_OPTIONS)).to.be.rejectedWith(Error)
+      expect(contract.setApprovalForAll('', true, TX_OPTIONS)).to.be.rejectedWith(Error)
     })
 
     it('should throw an error if approved is not provided', async () => {
       const stub = sinon.stub(contract, 'send').withArgs('setApprovalForAll', [TEST_ADDRESS_1], TX_OPTIONS)
       stub.rejects()
 
-      expect(contract.send('setApprovalForAll', [TEST_ADDRESS_1], TX_OPTIONS)).to.be.rejectedWith(Error)
+      expect(contract.setApprovalForAll(TEST_ADDRESS_1, false, TX_OPTIONS)).to.be.rejectedWith(Error)
     })
 
     it('should throw an error if txOptions are not provided', async () => {
       const stub = sinon.stub(contract, 'send').withArgs('setApprovalForAll', [TEST_ADDRESS_1, true])
       stub.rejects()
 
-      expect(contract.send('setApprovalForAll', [TEST_ADDRESS_1, true])).to.be.rejectedWith(Error)
+      expect(contract.setApprovalForAll(TEST_ADDRESS_1, true)).to.be.rejectedWith(Error)
     })
 
     it('should throw an error if params are not provided', async () => {
       const stub = sinon.stub(contract, 'send').withArgs('setApprovalForAll', [])
       stub.rejects()
 
-      expect(contract.send('setApprovalForAll', [])).to.be.rejectedWith(Error)
+      expect(contract.setApprovalForAll('', false)).to.be.rejectedWith(Error)
+    })
+  })
+
+  describe('approve', () => {
+    it('should return the transaction', async () => {
+      const stub = sinon.stub(contract, 'send').withArgs('approve', [TEST_ADDRESS_1, TOKEN_GOLD], TX_OPTIONS)
+      stub.resolves()
+
+      await contract.approve(TEST_ADDRESS_1, TOKEN_GOLD, TX_OPTIONS)
+      expect(stub.calledOnce).to.be.true
+      expect(stub.callCount).to.be.equals(1)
+    })
+
+    it('should throw an error if to is not provided', async () => {
+      const stub = sinon.stub(contract, 'send').withArgs('approve', ['', TOKEN_GOLD], TX_OPTIONS)
+      stub.rejects()
+
+      expect(contract.approve('', TOKEN_GOLD, TX_OPTIONS)).to.be.rejectedWith(Error)
+    })
+
+    it('should throw an error if tokenId is not provided', async () => {
+      const stub = sinon.stub(contract, 'send').withArgs('approve', [TEST_ADDRESS_1, ''], TX_OPTIONS)
+      stub.rejects()
+
+      expect(contract.approve(TEST_ADDRESS_1, '', TX_OPTIONS)).to.be.rejectedWith(Error)
+    })
+
+    it('should throw an error if txOptions are not provided', async () => {
+      const stub = sinon.stub(contract, 'send').withArgs('approve', [TEST_ADDRESS_1, TOKEN_GOLD])
+      stub.rejects()
+
+      expect(contract.approve(TEST_ADDRESS_1, TOKEN_GOLD)).to.be.rejectedWith(Error)
+    })
+
+    it('should throw an error if params are not provided', async () => {
+      const stub = sinon.stub(contract, 'send').withArgs('approve', [])
+      stub.rejects()
+
+      expect(contract.approve('', '')).to.be.rejectedWith(Error)
     })
   })
 })
